191ab241d487ec42b00766be36b1d0b078815c51
System Administrator <root@ashutoshs-MacBook-Pro.local>
2023-01-15 01:42:41 +0530

    bansal frontend intiialization

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import Nav from "./components/nav/Nav";
+import Home from "./components/home/Home";
+import Products from "./components/dryfruits/products/Products";
+import Contact from "./components/contact/Contact";
+import Login from "./components/login/Login";
+import About from "./components/about/About";
+import NotFound from "./components/notfound/NotFound";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import { BrowserRouter as Route } from "react-router-dom";
+
+import Footer from "./components/footer/Footer";
+
 function App() {
   return (
-    <div>
-      This application is for Bansal Traders
-    </div>
+    <>
+      <Router>
+        <Nav />
+        <Routes>
+          <Route activeClassName="active_class" exact path="/" element={<Home />}></Route>
+          <Route activeClassName="active_class" exact path="/products" element={<Products />}></Route>
+          <Route activeClassName="active_class" exact path="/about" element={<About />}></Route>
+          <Route activeClassName="active_class" exact path="/contact" element={<Contact />}></Route>
+          <Route activeClassName="active_class" exact path="/login" element={<Login />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+        <Footer />
+      </Router>
+    </>
   );
 }
 
---
+++ src/services/CategoryService.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const CATEGORY_API_BASE_URL = "http://localhost:8082/v1/categories";
+
+class CategoryService {
+  saveCategory(category) {
+    return axios.post(CATEGORY_API_BASE_URL, category);
+  }
+
+  getCategories() {
+    return axios.get(CATEGORY_API_BASE_URL);
+  }
+
+  deleteCategory(id) {
+    return axios.delete(CATEGORY_API_BASE_URL + "/" + id);
+  }
+
+  updateCategory(category, id) {
+    return axios.put(CATEGORY_API_BASE_URL + "/" + id, category);
+  }
+}
+
+export default new CategoryService();
