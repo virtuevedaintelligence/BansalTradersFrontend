f566bdefc00c57e19e1bed7d399d0a5e3defecf3
divinedryfruitsandfoods <rushikeshkarkhanis@Rushikeshs-MacBook-Pro.local>
2023-01-15 02:00:42 +0530

    Bansal Frontend Working

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Nav from "./components/nav/Nav";
 import Home from "./components/home/Home";
 import Products from "./components/dryfruits/products/Products";
@@ -5,10 +6,9 @@ import Contact from "./components/contact/Contact";
 import Login from "./components/login/Login";
 import About from "./components/about/About";
 import NotFound from "./components/notfound/NotFound";
+import Footer from "./components/footer/Footer";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 // import { BrowserRouter as Route } from "react-router-dom";
-
-import Footer from "./components/footer/Footer";
 
 function App() {
   return (
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <>
+            This is Footer
+        </>
+    )
+}
+
+export default Footer
