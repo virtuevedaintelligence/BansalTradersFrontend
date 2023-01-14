1bc22dbc9999ffc6123833865166dd0efe7c06f7
System Administrator <root@ashutoshs-MacBook-Pro.local>
2023-01-15 02:53:02 +0530

    Home Page changes 3AM

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -2,27 +2,30 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
-
+import "./nav.css";
+import "./responsive.css";
 import React from "react";
 
 function Topnavbar() {
   return (
     <>
-      <Navbar bg="light" expand="lg">
+      <Navbar expand="lg" className="nav-bg">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home">Bansal Traders</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
               <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown title="Products" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Dry Fruits</NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Spices</NavDropdown.Item>
               </NavDropdown>
+              <Nav.Link href="#link">Contact</Nav.Link>
+              <Nav.Link href="#link">About</Nav.Link>
+              <Nav.Link className="loginNavLink" href="#link">
+                Login
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
