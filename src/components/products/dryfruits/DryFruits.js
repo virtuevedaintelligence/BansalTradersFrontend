bfc457c546cb58a74518e4b380059295f33dc6ec
divinedryfruitsandfoods <rushikeshkarkhanis@Rushikeshs-MacBook-Pro.local>
2023-01-17 21:37:38 +0530

    Context implementation fuck all code

--- src/components/products/dryfruits/DryFruits.js
+++ src/components/products/dryfruits/DryFruits.js
@@ -1,70 +1,56 @@
 import React, { useEffect, useState } from "react";
-import ProductService from "../../../services/ProductService";
 import CategoryService from "../../../services/CategoryService";
 import { MDBContainer, MDBRow } from "mdb-react-ui-kit";
 import DryFruit from "./DryFruit";
 import DryFruitDetail from "./DryFruitDetail";
+import { ProductContext, useProductContext } from "../../../context/productcontext"
 
-function DryFruits() {
-  const [loading, setLoading] = useState(true);
-  const [products, setProducts] = useState(null);
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const response = await ProductService.getProducts();
-        console.log(response);
-        setProducts(response.data);
-      } catch (error) {
-        console.log(error);
-      }
-      setLoading(false);
-    };
-    fetchData();
-  }, []);
+const DryFruits = () => {
+    const { isLoading, products } = useProductContext();
+    const [Catloading, CatsetLoading] = useState(true);
+    const [categories, setCategory] = useState(null);
 
-  const [Catloading, CatsetLoading] = useState(true);
-  const [categories, setCategory] = useState(null);
-  useEffect(() => {
-    const fetchData = async () => {
-      CatsetLoading(true);
-      try {
-        const response1 = await CategoryService.getCategories();
-        console.log(response1.data);
-        setCategory(response1.data);
-      } catch (error) {
-        console.log(error);
-      }
-      CatsetLoading(false);
-    };
-    fetchData();
-  }, []);
-  return (
-    <>
-      <div className="scrollmenu">
-        {!Catloading && (
-          <div>
-            {categories.map((category) => (
-              <a key={category.id} href="#home">
-                {category.categoryName}
-              </a>
-            ))}
-          </div>
-        )}
-      </div>
-      <MDBContainer fluid className="my-5">
-        {!loading && (
-          <MDBRow>
-            {products.map((product) => (
-              <DryFruit key={product.productId} product={product}></DryFruit>
-            ))}
-          </MDBRow>
-        )}
-      </MDBContainer>
+    useEffect(() => {
+        const fetchData = async () => {
+            CatsetLoading(true);
+            try {
+                const response1 = await CategoryService.getCategories();
+                console.log(response1.data);
+                setCategory(response1.data);
+            } catch (error) {
+                console.log(error);
+            }
+            CatsetLoading(false);
+        };
+        fetchData();
+    }, []);
+    if (isLoading) {
+        return <div>... Loading</div>
+    }
+    return (
+        <>
+            <div className="scrollmenu">
+                {!Catloading && (
+                    <div>
+                        {categories.map((category) => (
+                            <a key={category.id} href="#home">
+                                {category.categoryName}
+                            </a>
+                        ))}
+                    </div>
+                )}
+            </div>
+            <MDBContainer fluid className="my-5">
+                <MDBRow>
+                    {products.map((product) => {
+                        <DryFruit key={product.productId} {...product} />
+                    })}
+                </MDBRow>
+            </MDBContainer>
 
-      {/* <DryFruitDetail /> */}
-    </>
-  );
+            {/* <DryFruitDetail /> */}
+        </>
+    );
 }
 
 export default DryFruits;
---
+++ src/context/productcontext.js
@@ -0,0 +1,47 @@
+import { createContext, useContext, useEffect, useReducer } from "react";
+import ProductService from "../services/ProductService";
+import reducer from "../reducer/productReducer";
+
+const ProductContext = createContext();
+const initialState = {
+    isLoading: false,
+    isError: false,
+    products: [],
+    featuredProducts: []
+};
+
+const ProductProvider = ({ children }) => {
+
+    const [state, dispatch] = useReducer(reducer, initialState);
+    const fetchData = async () => {
+        dispatch({ type: "PRODUCT_LOADING" });
+        try {
+            const response = await ProductService.getProducts();
+            console.log(response);
+            const products = await response.data;
+            console.log(products);
+            dispatch({ type: "SET_PRODUCT_DATA", payload: products });
+        } catch (error) {
+            console.log(error);
+            dispatch({ type: "PRODUCT_ERROR" });
+        }
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    return (
+        <ProductContext.Provider value={{ ...state }}>
+            {children}
+        </ProductContext.Provider >
+    );
+};
+
+//custom hook
+
+const useProductContext = () => {
+    return useContext(ProductContext);
+};
+
+export { ProductProvider, ProductContext, useProductContext };
