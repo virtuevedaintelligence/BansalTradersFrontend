ba9b74b9258f2170d9ea7b96651d02be148d6f4c
kolambkar11 <root@ashutoshs-MacBook-Pro.local>
2023-01-18 01:01:56 +0530

    fixes done home page and product

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,16 +1,17 @@
 import React from "react";
-import Featuredproducts from "./Featuredproducts";
 import Slider from "./Slider";
 import { MDBContainer, MDBRow } from "mdb-react-ui-kit";
 import DryFruit from "../products/dryfruits/DryFruit";
+import { useProductContext } from "../../context/productcontext";
+import Shopbycategory from "./Shopbycategory";
 
 export default function Home() {
   const { isLoading, featuredProducts } = useProductContext();
   if (isLoading) {
-    return <div>... Loading</div>
+    return <div>... Loading</div>;
   }
-  console.log(featuredProducts)
-  console.log("Home")
+  console.log(featuredProducts);
+  console.log("Home");
   return (
     <>
       <Slider />
@@ -18,7 +19,7 @@ export default function Home() {
       <MDBContainer fluid className="my-5">
         <MDBRow>
           {featuredProducts.map((product) => {
-            <DryFruit key={product.productId} {...product} />
+            <DryFruit key={product.productId} {...product} />;
           })}
         </MDBRow>
       </MDBContainer>
