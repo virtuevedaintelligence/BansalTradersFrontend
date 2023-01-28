65741c8b33031ea5f78033182958da5cc23ad2bc
divinedryfruitsandfoods <rushikeshkarkhanis@Rushikeshs-MacBook-Pro.local>
2023-01-28 11:56:52 +0530

    Initial commit for More Products

--- src/components/products/moreproducts/MoreProducts.js
+++ src/components/products/moreproducts/MoreProducts.js
@@ -1,14 +1,19 @@
 import React from "react";
+import { useProductContext } from "../../../context/productcontext";
 import MoreProduct from "./MoreProduct";
 
 function MoreProducts() {
+  const { isLoading, products } = useProductContext();
+
+  if (isLoading) {
+    return <div>... Loading</div>;
+  }
   return (
     <>
       <div className="row mt-3 p-0 text-center pro-box-section">
-        <MoreProduct />
-        <MoreProduct />
-        <MoreProduct />
-        <MoreProduct />
+        {products.filter(product => product.categoryName === {}).map((product) => {
+          return <MoreProduct key={product.productId} product={product} />;
+        })}
       </div>
     </>
   );
--- src/components/reviews/StarRating.js
+++ src/components/reviews/StarRating.js
@@ -4,7 +4,8 @@ import React from 'react'
 
 function StarRating() {
     return (
-        <div>StarRating</div>
+        <>
+        </>
     )
 }
 
