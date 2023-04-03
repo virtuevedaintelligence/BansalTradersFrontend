99bc2b122b632e7eebae53c853eff7646e81bab3
kolambkar11 <root@VVI.local>
2023-04-03 11:30:49 +0530

    click issues in admin categroy update and delete

--- src/components/products/admin/UpdateDeleteCategory.js
+++ src/components/products/admin/UpdateDeleteCategory.js
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import UpdateProduct from "../UpdateProduct";
 import { MdDelete } from "react-icons/md";
 import { useProductContext } from "../../../context/productcontext";
 import { FiEdit2 } from "react-icons/fi";
 import { useCategoryContext } from "../../../context/categorycontext";
+
+import { Button, Form, Modal, Row } from "react-bootstrap";
 
 function UpdateDeleteCategory({ category, categoryId }) {
   const [show, setShow] = useState(false);
@@ -13,8 +15,13 @@ function UpdateDeleteCategory({ category, categoryId }) {
     e.preventDefault();
     deleteProductCall(categoryId);
   };
+
   const { id, categoryName, categoryType } = category;
   const { isDeleteCategoryLoading, deleteCategoryCall, isUpdateCategoryLoading, updateCategoryCall, fetchCategory } = useCategoryContext();
+  const [categoryToUpdate, setCategoryToUpdate] = useState({
+    categoryName: categoryName,
+    categoryType: categoryType,
+  });
 
   const handleShow = () => setShow(true);
   const deleteCat = (e) => {
@@ -22,6 +29,28 @@ function UpdateDeleteCategory({ category, categoryId }) {
     deleteCategoryCall(id);
     fetchCategory();
   };
+  const handleClose = () => setShow(false);
+
+  useEffect(() => {
+    fetchCategory();
+  }, []);
+  const updateCat = (e) => {
+    e.preventDefault();
+    updateCategoryCall(id, categoryToUpdate);
+    handleClose();
+    fetchCategory();
+  };
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setCategoryToUpdate({ ...categoryToUpdate, [e.target.name]: value });
+  };
+  if (isDeleteCategoryLoading) {
+    return <Preloader />;
+  }
+  if (isUpdateCategoryLoading) {
+    return <Preloader />;
+  }
+
   const adminData = useSelector((state) => {
     return state.admin;
   });
@@ -35,6 +64,36 @@ function UpdateDeleteCategory({ category, categoryId }) {
         <button className="btn-sm btn btn-danger">
           <MdDelete onClick={deleteCat} />
         </button>
+        <Modal show={show} onHide={handleClose} backdrop="static" keyboard={false}>
+          <Modal.Header closeButton>
+            <Modal.Title>Update Category</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <div className="container text-center">
+              <Row className="mb-3">
+                <Form.Group className="col col-sm-12">
+                  <Form.Control type="name" name="categoryName" placeholder="Category Name" className="form-control-sm mb-2" defaultValue={categoryName} onChange={(e) => handleChange(e)} />
+                </Form.Group>
+                <Form.Group className="col col-sm-12">
+                  <Form.Select name="categoryType" className="form-control-sm" size="sm" onChange={(e) => handleChange(e)} defaultValue={categoryType}>
+                    <option value="Dryfruit">Dryfruit</option>
+                    <option value="Spices">Spices</option>
+                  </Form.Select>
+                </Form.Group>
+              </Row>
+              <Row className="mb-3">
+                <Form.Group className="col col-sm-12">
+                  <button type="submit" className="me-4 btn btn-success btn-sm btn-block" onClick={updateCat}>
+                    Update
+                  </button>
+                  <button type="reset" className="me-4 btn btn-danger btn-sm btn-block" onClick={handleClose}>
+                    Cancel
+                  </button>
+                </Form.Group>
+              </Row>
+            </div>
+          </Modal.Body>
+        </Modal>
       </>
     );
   } else {
--- src/components/products/categories/Category.js
+++ src/components/products/categories/Category.js
@@ -1,7 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { Button, Form, Modal, Row } from "react-bootstrap";
-import { FiEdit2 } from "react-icons/fi";
-import { MdDelete } from "react-icons/md";
 import { useSelector } from "react-redux";
 import { useCategoryContext } from "../../../context/categorycontext";
 import { useFilterContext } from "../../../context/fitercontext";
@@ -12,40 +9,8 @@ function Category({ category, filters, filterDryfruits }) {
   const adminData = useSelector((state) => {
     return state.admin;
   });
-  const [show, setShow] = useState(false);
-  const { isDeleteCategoryLoading, deleteCategoryCall, isUpdateCategoryLoading, updateCategoryCall, fetchCategory } = useCategoryContext();
-  const { id, categoryName, categoryType } = category;
-  const [categoryToUpdate, setCategoryToUpdate] = useState({
-    categoryName: categoryName,
-    categoryType: categoryType,
-  });
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-  const deleteCat = (e) => {
-    e.preventDefault();
-    deleteCategoryCall(id);
-    fetchCategory();
-  };
-
-  useEffect(() => {
-    fetchCategory();
-  }, []);
-  const updateCat = (e) => {
-    e.preventDefault();
-    updateCategoryCall(id, categoryToUpdate);
-    handleClose();
-    fetchCategory();
-  };
-  const handleChange = (e) => {
-    const value = e.target.value;
-    setCategoryToUpdate({ ...categoryToUpdate, [e.target.name]: value });
-  };
-  if (isDeleteCategoryLoading) {
-    return <Preloader />;
-  }
-  if (isUpdateCategoryLoading) {
-    return <Preloader />;
-  }
+ 
+  
 
   if (adminData.dataAdminLogin) {
   }
@@ -61,36 +26,7 @@ function Category({ category, filters, filterDryfruits }) {
         <MdDelete onClick={deleteCat} />
       </button> */}
 
-      <Modal show={show} onHide={handleClose} backdrop="static" keyboard={false}>
-        <Modal.Header closeButton>
-          <Modal.Title>Update Category</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <div className="container text-center">
-            <Row className="mb-3">
-              <Form.Group className="col col-sm-12">
-                <Form.Control type="name" name="categoryName" placeholder="Category Name" className="form-control-sm mb-2" defaultValue={categoryName} onChange={(e) => handleChange(e)} />
-              </Form.Group>
-              <Form.Group className="col col-sm-12">
-                <Form.Select name="categoryType" className="form-control-sm" size="sm" onChange={(e) => handleChange(e)} defaultValue={categoryType}>
-                  <option value="Dryfruit">Dryfruit</option>
-                  <option value="Spices">Spices</option>
-                </Form.Select>
-              </Form.Group>
-            </Row>
-            <Row className="mb-3">
-              <Form.Group className="col col-sm-12">
-                <button type="submit" className="me-4 btn btn-success btn-sm btn-block" onClick={updateCat}>
-                  Update
-                </button>
-                <button type="reset" className="me-4 btn btn-danger btn-sm btn-block" onClick={handleClose}>
-                  Cancel
-                </button>
-              </Form.Group>
-            </Row>
-          </div>
-        </Modal.Body>
-      </Modal>
+      
     </>
   );
 }
